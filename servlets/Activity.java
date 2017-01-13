b7f087a46fbb3ea9b61158e0a15aef133b993ee4
raman049 <raman049@yahoo.com>
2017-01-13 12:24:49 -0800

    Update Activity.java

--- servlets/Activity.java
+++ servlets/Activity.java
@@ -1,12 +1,11 @@
 package servlets;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -16,7 +15,7 @@ public class Activity implements ActionListener {
 	public static Activity activity;
 	FrameClass jframe;
 	int HEIGHT, WIDTH;
-	Rectangle boat;
+	Rectangle Recatangleboat;
 	Boolean gameOver;
 	public Render render;
 	public Random random;
@@ -26,39 +25,39 @@ public class Activity implements ActionListener {
 		HEIGHT = jframe.getHeight();
 		WIDTH = jframe.getWidth();
 		Timer timer = new Timer(20, this);
-		boat = new Rectangle(jframe.getWidth() / 3, jframe.getHeight() / 2 - 10, 50, 30);
+		Recatangleboat = new Rectangle(jframe.getWidth() / 3, jframe.getHeight() / 2 - 10, 50, 30);
 		render = new Render();
 		random = new Random();
 		jframe.add(render);
 		jframe.setVisible(true);
-		jframe.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_F2) {
-					// close frame one.
-				}
-			}
-		});
-		;
-
 		timer.start();
 
 	}
 
 	public void Repaint(Graphics g) {
-		g.setColor(new Color(153, 204, 255)); // background color
-		g.fillRect(0, 0, WIDTH, HEIGHT);
-		g.setColor(new Color(153, 204, 255)); // sky color
-		g.fillRect(0, 0, WIDTH, HEIGHT / 6);
-
+		g.setFont(new Font("Comic Sans MS", 1, 25));
+		g.setColor(new Color(255, 0, 0));
+		g.drawString("High Score: ", WIDTH / 2 - 100, HEIGHT / 10);
+		g.setFont(new Font("Comic Sans MS", 2, 100));
+		g.setColor(new Color(255, 255, 0));
+		g.drawString("Tap to Start", WIDTH / 2 - 275, HEIGHT / 2 - 50);
+		
+//		g.setColor(new Color(153, 204, 255)); // background color
+//		g.fillRect(0, 0, 100, 1000);
+//		g.setColor(new Color(153, 204, 255)); // sky color
+//		g.fillRect(0, 0, WIDTH, HEIGHT / 6);
+		
 	}
 
 	public class Render extends JPanel {
-		private static final long serialVersionUID = 1L;
 
-		@Override
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			Activity.activity.Repaint(g);
+			Repaint(g);			
 		}
 	}
 
@@ -72,7 +71,7 @@ public class Activity implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
