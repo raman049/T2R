19c3eba89b7a47f630a24e5757a23e45578e6ebc
shekharchaudhary <shekharchaudhary@users.noreply.github.com>
2017-01-13 14:05:00 -0800

    Update InitialPage.java

--- InitialPage.java
+++ InitialPage.java
@@ -1,156 +1,65 @@
 
+
 package T2R;
 
-import java.awt.Button;
 import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
 
 public class InitialPage {
 
-	private static JLabel lable;
-	private static JButton newbutton;
-	private static JButton btnNewButton;
-	static FrameClass frame = new FrameClass();
-	static HighScore hiScore;
-	static int Height;
-	static int Width;
-	static Boolean playClip = true;
-	static Clip clip;
-	static BufferedImage background;
+	public BufferedImage image;
+	static FrameClass jframe;
+	static int HEIGHT;
+	static int WIDTH;
+	public static Render render;
 
-	/**
-	 * Launch the application.
-	 */
+	InitialPage() {
+	}
+
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@SuppressWarnings("static-access")
-			public void run() {
-				try {
-					InitialPage window = new InitialPage();
-					window.frame.setVisible(true);
-					clip = AudioSystem.getClip();
-					clip.open(AudioSystem.getAudioInputStream(new File("SoundClips/latinHorn.wav")));
-					clip.loop(clip.LOOP_CONTINUOUSLY);
-					clip.start();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+
+		InitialPage a = new InitialPage();
+		a.fristPage();
 
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public InitialPage() {
-		initialize();
+	public void fristPage() {
+		jframe = new FrameClass();
+		HEIGHT = jframe.getHeight();
+		WIDTH = jframe.getWidth();
+		render = new Render();
+		jframe.add(render);
+		jframe.setVisible(true);
+
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		
-		Height = frame.getHeight();
-		Width = frame.getWidth();
-		frame.setBackground(new Color(127, 255, 212));
-		frame.setLayout(null);
-		frame.setResizable(false);
-		
+	public void Repaint(Graphics g) {
+		// g.setFont(new Font("Comic Sans MS", 1, 25));
+		g.setColor(new Color(255, 0, 0));
+		g.drawString("High Score: ", WIDTH / 2 - 100, HEIGHT / 10);
 		try {
-			background = ImageIO.read(new File("drum1.png"));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		JLabel imagelabel = new JLabel(new ImageIcon(background));
-		imagelabel.setBounds(Width / 2 , Height / 2 , Width / 2, Width/2);
-		frame.add(imagelabel);
-		
-		
-		
-		
-		
-		//Button To ROW
-
-		JButton btnNewButton = new JButton("ROW");
-		btnNewButton.setForeground(new Color(139, 0, 0));
-		btnNewButton.addActionListener(new Action());
-		btnNewButton.setBackground(Color.LIGHT_GRAY);
-		btnNewButton.setFont(new Font("Comic Sans MS", Font.BOLD, Height / 50));
-		btnNewButton.setBounds(Width / 3, Height / 2, Height / 4, Height / 32);
-		((JFrame) frame).getContentPane().add(btnNewButton);
-		
-		//Button To Option
-
-		JButton newbutton = new JButton("INSTRUCTION");
-		newbutton.setForeground(new Color(139, 0, 0));
-		newbutton.addActionListener(new Action());
-		newbutton.setBackground(Color.LIGHT_GRAY);
-		newbutton.setFont(new Font("Comic Sans MS", Font.BOLD, Height / 50));
-		newbutton.setBounds(Width / 3, Height / 2+100, Height / 4, Height / 32);
-		((JFrame) frame).getContentPane().add(newbutton);
-		
-		
-		
-
-		// High Score
-		
-		
-		String HIscore = "";
-		try {
-			HIscore = HighScore.updateHiScore(0).toString();
+			image = ImageIO.read(ResourceLoader.load("T2Rimages/drum1.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		lable = new JLabel();
-		lable.setFont(new Font("Comic Sans MS", Font.BOLD, Height / 32));
-		lable.setText("High Score : " + HIscore);
-		lable.setForeground(new Color(139, 0, 0));
-		lable.setBounds(Width / 3, Height / 2 , Width / 2, Height / 2);
-		frame.add(lable);
+		g.drawImage(image, WIDTH / 2, HEIGHT / 2, 200, 200, null);
+
 	}
 
-	static class Action implements ActionListener {
+	public class Render extends JPanel {
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				clip.close();// close music
-				Thread.sleep(500);// pause for 900 msec
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			frame.setVisible(false);
-			MainActivity a = new MainActivity();
-			a.ActivityMethod();
-			
-			
+		private static final long serialVersionUID = 1L;
+
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Repaint(g);
 		}
-		
 	}
+
 }
